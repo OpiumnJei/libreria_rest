bf6ca95876d7b238a4c4994b69bb2fad8f579e8d
Jerlinson Gonzalez <jerlinsongonzalez0000@gmail.com>
2024-08-16 20:53:21 -0400

    added jsonProperties

--- src/main/java/com/prueba_tecnica/libreria/domain/book/Gender.java
+++ src/main/java/com/prueba_tecnica/libreria/domain/book/Gender.java
@@ -9,18 +9,39 @@ public enum Gender {
     @JsonProperty("no fiction")
     NON_FICTION,
 
+    @JsonProperty("mistery")
     MYSTERY,
+
+    @JsonProperty("fantasy")
     FANTASY,
+
+    @JsonProperty("science fiction")
     SCIENCE_FICTION,
+
+    @JsonProperty("biography")
     BIOGRAPHY,
+
+    @JsonProperty("history")
     HISTORY,
+
+    @JsonProperty("romance")
     ROMANCE,
+
+    @JsonProperty("thriller")
     THRILLER,
+
+    @JsonProperty("horror")
     HORROR,
 
     @JsonProperty("poetry")
     POETRY,
+
+    @JsonProperty("adventure")
     ADVENTURE,
+
+    @JsonProperty("drama")
     DRAMA,
+
+    @JsonProperty("clasic")
     CLASSIC
 }
